3cf2110a71109c7dba3bc8a9c5a5db4eea2287bf
as.yarchuk <as.yarchuk@gmail.com>
2022-11-02 18:09:35 +0200

    prod test

--- sequelize/db.js
+++ sequelize/db.js
@@ -1,6 +1,3 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize({
-    dialect: 'postgres',
-    url: 'postgres://admin:J4zryWAYc5Pmw0BvQjRwPEhK5VmDmXxm@dpg-cdh5tjpgp3jpn59skq10-a.frankfurt-postgres.render.com/workportal'
-  });
+module.exports = new Sequelize('postgres://admin:J4zryWAYc5Pmw0BvQjRwPEhK5VmDmXxm@dpg-cdh5tjpgp3jpn59skq10-a.frankfurt-postgres.render.com/workportal');
